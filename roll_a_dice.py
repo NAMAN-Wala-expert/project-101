bb173e76d7b74684217f6b5cfaabf8f7a2e87216
NAMAN-Wala-expert <107202669+NAMAN-Wala-expert@users.noreply.github.com>
2024-01-21 21:12:13 +0530

    Add files via upload

--- roll_a_dice.py
+++ roll_a_dice.py
@@ -7,9 +7,38 @@ while (response != "n"):
     no = random.randint(1,6)
     if  response != "n" :
         print(no)
+        if  no == 1:
+         print("[     ]")
+         print("[  0  ]")
+         print("[     ]")
+        elif no == 2:
+         print("[     ]")
+         print("[ 0 0 ]")
+         print("[     ]")
+        elif no == 3:
+         print("[  0  ]")
+         print("[  0  ]")
+         print("[  0  ]")
+        elif no == 4:
+         print("[0   0]")
+         print("[     ]")
+         print("[0   0]")
+        elif no == 5:         
+         print("[0   0]")
+         print("[  0  ]")
+         print("[0   0]")
+        else :      
+         print("[0   0]")
+         print("[0   0]")
+         print("[0   0]")
+         
         response = input("write y for yes and n for exit")
     else:
             print("thank you for using this app")
+    
+         
+         
+
 
  
 
